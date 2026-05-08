3905e9a25f5074a6a677bff13e60e1b63758dbb5
Andika Pramudya Putra <andika.putra@benerit.id>
2026-05-08 15:35:01 +0700

    Use standard Vercel API entrypoint

--- api/index.ts
+++ api/index.ts
@@ -2,10 +2,7 @@ import { handle } from "@hono/node-server/vercel";
 import app from "./boot";
 
 export const config = {
-  runtime: "nodejs",
   maxDuration: 60,
 };
 
-const handler = handle(app);
-
-export default handler;
+export default handle(app);
